d818b3b6ae67267c1312cc8eb9abd2a4f6ec604c
salex139s <sasha.13.9@yandex.ru>
2024-11-25 19:59:19 +0300

    logs

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -20,41 +20,54 @@ func main() {
 		}
 	}(file)
 	log.SetOutput(file)
+	log.Println("INFO: Initialized log")
 
 	// Create new client
+	log.Println("INFO: Creating client")
 	c, err := client.NewClient()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+	log.Println("INFO: Created client")
 
+	log.Println("INFO: Initializing client")
 	// Init client
 	err = c.Init()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+	log.Println("INFO: Initialized client")
 
 	// Connect to another user
+	log.Println("INFO: Choosing mode")
 	fmt.Println("Choose mode (listen, connect): ")
 	var mode string
 	_, err = fmt.Scanln(&mode)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	if mode == "listen" {
+		log.Println("INFO: Mode listen")
 		err = c.Listen()
 		if err != nil {
-			panic(err)
+			log.Panic(err)
 		}
 	} else if mode == "connect" {
+		log.Println("INFO: Mode connect")
 		err = c.Connect()
 		if err != nil {
-			panic(err)
+			log.Panic(err)
 		}
+	} else {
+		log.Panic("wrong mode")
 	}
+	log.Println("INFO: Connected host")
 
 	// Serve client
+	log.Println("INFO: Serving")
 	err = c.Serve()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+	log.Println("INFO: Exiting")
 }
--- internal/vpn/connector.go
+++ internal/vpn/connector.go
@@ -3,7 +3,6 @@ package vpn
 import (
 	"log"
 	"net"
-	"time"
 )
 
 const BuffSize int = 65535 // UDP packet max size
@@ -14,59 +13,77 @@ type Connector struct {
 }
 
 func (c *Connector) SendAll(msg []byte) {
+	log.Println("INFO: Sending started")
 	for _, conn := range c.conns {
+		log.Println("INFO: Sending message to ", conn.RemoteAddr())
 		_, err := conn.Write(msg)
 		if err != nil {
 			log.Printf("ERROR: Write message error to host %v: %v", conn.RemoteAddr(), err)
 			continue
 		}
+		log.Println("INFO: Sent message to ", conn.RemoteAddr())
 	}
+	log.Println("INFO: Sending finished")
 }
 
 func (c *Connector) handler(conn *net.UDPConn) {
-	time.Sleep(3 * time.Second)
 	buff := make([]byte, BuffSize)
+	log.Println("INFO: Handling from ", conn.RemoteAddr())
 	for {
 		n, err := conn.Read(buff)
 		if err != nil {
+			// TODO check if connection closed
 			log.Printf("ERROR: Read message error from %v: %v", conn.RemoteAddr(), err)
+			continue
 			//log.Printf("INFO: Connection to %v closed\n", conn.RemoteAddr())
 			//return
 		}
 		c.Input <- buff[:n]
+		log.Println("INFO: Read message from ", conn.RemoteAddr())
 	}
 }
 
 func (c *Connector) connect(iaddr, raddr *net.UDPAddr) (*net.UDPConn, error) {
+	log.Printf("INFO: Dial from %v to %v\n", iaddr, raddr)
 	conn, err := net.DialUDP("udp", iaddr, raddr)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("INFO: Created dial to", conn.RemoteAddr())
+	log.Println("INFO: Staring handler ", conn.RemoteAddr())
 	go c.handler(conn)
 	c.conns = append(c.conns, conn)
 	return conn, nil
 }
 
 func (c *Connector) Listen(iaddr *net.UDPAddr) (*net.UDPConn, error) {
+	log.Println("INFO: Listening on", iaddr)
 	conn, err := net.ListenUDP("udp", iaddr)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("INFO: Created listener on", conn.LocalAddr())
 
+	log.Println("INFO: Waiting connection on", conn.LocalAddr())
 	_, raddr, err := conn.ReadFromUDP(nil)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("INFO: Received connection from", raddr)
 
+	log.Println("INFO: Closing listener on", conn.LocalAddr())
 	err = conn.Close()
 	if err != nil {
 		return nil, err
 	}
+	log.Println("INFO: Listener closed on", conn.LocalAddr())
 
+	log.Printf("INFO: Connecting from %v to %v\n", iaddr, raddr)
 	return c.connect(iaddr, raddr)
 }
 
 func (c *Connector) Connect(raddr *net.UDPAddr) (*net.UDPConn, error) {
+	log.Println("INFO: Connecting to", raddr)
 	return c.connect(nil, raddr)
 }
 
